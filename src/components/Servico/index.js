41be78ce9f35f99d8c2e65ce76146f01a69dd4de
BLKNeko <blackheart2302@gmail.com>
2021-10-06 15:38:16 -0300

    teste my code

--- src/components/Servico/index.js
+++ src/components/Servico/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import PubSub from 'pubsub-js';
 
-import { Table, Button, Form, FormGroup, Label, Input  } from 'reactstrap';
+import { Table, Button, Form, FormGroup, Label, Input, Alert  } from 'reactstrap';
 
 class FormServ extends Component {
 
@@ -13,12 +14,14 @@ class FormServ extends Component {
     }
 
     create = () => {
-        let data = {
-            id: parseInt(this.state.model.id_servico),
-            nome: this.state.model.nome,
-        };
-        console.log(data)
-        this.props.servicesCreate(data);
+        this.setState({ model: {id_servico: 0, nome: ''} })
+        this.props.servicesCreate(this.state.model);
+    }
+
+    componentDidMount(){
+        PubSub.subscribe('edit-service', (topic, service) =>{
+            this.setState({model: service});
+        });
     }
 
 
@@ -53,6 +56,10 @@ class ListServ extends Component {
         this.props.deleteServices(id_servico);
     }
 
+    onEdit = (services) => {
+        PubSub.publish('edit-service', services);
+    }
+
     
     render() {
 
@@ -74,7 +81,7 @@ class ListServ extends Component {
                                 <td>{services.id_servico}</td>
                                 <td>{services.nome}</td>
                                 <td>
-                                    <Button color="info" size="sm">Editar</Button>
+                                    <Button color="info" size="sm" onClick={e=> this.onEdit(services)}>Editar</Button>
                                     <Button color="danger" size="sm" onClick={e => this.delete(services.id_servico)}>Deletar</Button>
                                 </td>
                             </tr>
@@ -93,6 +100,7 @@ export default class ServBox extends Component {
 
     state = {
         services: [],
+        message: { text: '', alert: '' }
     }
 
     componentDidMount() {
@@ -103,22 +111,47 @@ export default class ServBox extends Component {
 
     }
 
-    create = (services) => {
+    save = (services) => {
+        let data = {
+            id: parseInt(services.id_servico),
+            nome: services.nome,
+        };
+        console.log(data);
         const requestInfo = {
-            method: 'POST',
-            body: JSON.stringify(services),
+            method: data.id !== 0? 'PATCH': 'POST' ,
+            body: JSON.stringify(data),
             headers: new Headers({
                 'Content-type': 'application/json'
             })
         };
-        fetch(this.Url, requestInfo)
+
+        if(data.id === 0){
+            //CREATE
+            fetch(this.Url, requestInfo)
             .then(response => response.json())
             .then(newService => {
                 let { services} = this.state;
                 services.push(newService);
-                this.setState({services});
+                this.setState({services, message: { text: 'Serviço cadastrado com sucesso!', alert: 'success' }});
+                this.timerMessage(3000);
             })
             .catch(e => console.log(e));
+        } else {
+            //EDIT
+            fetch(`${this.Url}/${data.id}`, requestInfo)
+            .then(response => response.json())
+            .then(updatedService => {
+                let { services} = this.state;
+                let position = services.findIndex(services => services.id_servico=== data.id);
+                services[position] = updatedService;
+                this.setState({services, message: { text: 'Serviço atualizado com sucesso!', alert: 'info' }});
+                this.timerMessage(3000);
+            })
+            .catch(e => console.log(e));
+
+        }
+
+        
     }
 
     delete = (id_servico) => {
@@ -126,23 +159,41 @@ export default class ServBox extends Component {
             .then(response => response.json())
             .then(rows => {
                 const services = this.state.services.filter(services => services.id_servico != id_servico);
-                this.setState({ services });
+                this.setState({ services, message: { text: 'Serviço deletado com sucesso!', alert: 'danger' } });
+                this.timerMessage(3000);
             })
             .catch(e => console.log(e));
+    }
+
+    timerMessage = (duration) => {
+        setTimeout(() => {
+            this.setState({ message: { text: '', alert: ''} });
+        }, duration);
+        
     }
 
 
     render() {
         return (
-            <div className="row">
-                <div className="col-md-6 my-3">
-                    <h2 className="font-weight-bold text-center"> Cadastro de Serviços</h2>
-                    <FormServ servicesCreate={this.create} />
-                </div>
-                <div className="col-md-6 my-3">
+            <div>
+                {
+                    this.state.message.text !== ''? (
+                        <Alert color={this.state.message.alert} className="text-center"> {this.state.message.text} </Alert>
+                    ) : ''
+                }
+            
+                <div className="row">
+                    
+                    <div className="col-md-6 my-3">
+                        <h2 className="font-weight-bold text-center"> Cadastro de Serviços</h2>
+                        <FormServ servicesCreate={this.save} />
+                    </div>
+                    <div className="col-md-6 my-3">
 
-                    <h2 className="font-weight-bold text-center"> Lista de Serviços</h2>
-                    <ListServ services={this.state.services} deleteServices={this.delete} />
+                        <h2 className="font-weight-bold text-center"> Lista de Serviços</h2>
+                        <ListServ services={this.state.services} deleteServices={this.delete} />
+                    </div>
+
                 </div>
 
             </div>
